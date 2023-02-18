7cdc6d6b332b329c042702050a85e6fdbf6e2a59
NusratJahanGeek <nusrat.jahan.4338@gmail.com>
2023-02-19 00:52:41 +0600

    random color added

---
+++ js/random-color.js
@@ -0,0 +1,15 @@
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  } 
+  const randomColorEffect = document.getElementById('random');
+  const randomColorEffect2 = document.getElementById('random-2');
+  randomColorEffect.addEventListener('mouseover', function(){
+    randomColorEffect.style.backgroundColor = randomColor();
+  });
+  randomColorEffect2.addEventListener('mouseover', function(){
+    randomColorEffect2.style.backgroundColor = randomColor();
+  });
