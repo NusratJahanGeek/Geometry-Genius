d1b8b2382beb9e583729db3febfff03ff7ab10da
NusratJahanGeek <nusrat.jahan.4338@gmail.com>
2023-02-19 23:04:40 +0600

    Edit Icon Added

---
+++ js/display.js
@@ -0,0 +1,15 @@
+const editField = document.getElementById('edit-field');
+
+document.getElementById('edit-icon').addEventListener('click', function () {
+    editField.style.display = 'flex';
+});
+
+const editField2 = document.getElementById('edit-field2');
+
+document.getElementById('edit-icon2').addEventListener('click', function () {
+    editField2.style.display = 'flex';
+});
+
+
+
+
--- js/random-color.js
+++ js/random-color.js
@@ -7,10 +7,26 @@ function randomColor() {
 } 
 const randomColorEffect = document.getElementById('random');
 const randomColorEffect2 = document.getElementById('random-2');
+const randomColorEffect3 = document.getElementById('random-3');
+const randomColorEffect4 = document.getElementById('random-4');
+const randomColorEffect5 = document.getElementById('random-5');
+const randomColorEffect6 = document.getElementById('random-6');
 
 randomColorEffect.addEventListener('mouseover', function(){
   randomColorEffect.style.backgroundColor = randomColor();
 });
 randomColorEffect2.addEventListener('mouseover', function(){
   randomColorEffect2.style.backgroundColor = randomColor();
+});
+randomColorEffect3.addEventListener('mouseover', function(){
+  randomColorEffect3.style.backgroundColor = randomColor();
+});
+randomColorEffect4.addEventListener('mouseover', function(){
+  randomColorEffect4.style.backgroundColor = randomColor();
+});
+randomColorEffect5.addEventListener('mouseover', function(){
+  randomColorEffect5.style.backgroundColor = randomColor();
+});
+randomColorEffect6.addEventListener('mouseover', function(){
+  randomColorEffect6.style.backgroundColor = randomColor();
 });
